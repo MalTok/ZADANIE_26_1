7f0b3fedfad105877bd472b76e2b0a5568dee651
MalTok <mig01@wp.pl>
2023-12-02 10:09:15 +0100

    Utworzenie zadania

---
+++ src/main/java/pl/mt/cookbook/Zadanie261Application.java
@@ -0,0 +1,13 @@
+package pl.mt.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie261Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie261Application.class, args);
+    }
+
+}
